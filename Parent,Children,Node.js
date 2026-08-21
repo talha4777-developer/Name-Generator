6016b8aa75438919448996c752a28fc7841e6614
CodeWithTalha <shineschool4777@gmail.com>
2026-08-21 22:51:12 +0500

    Add files via upload

---
+++ Parent,Children,Node.js
@@ -0,0 +1,49 @@
+// Target Container Heading
+let containerHeading = document.body.firstElementChild.children[0];
+containerHeading.style = "background: red; color: white; font-family: sans-serif; margin: 5px 5px";
+containerHeading.textContent = "Learning HTML, CSS, JavaScript";
+
+// Target Box 1 && His Child
+let containerBox1 = document.body.firstElementChild.children[1];
+containerBox1.style = "text-align: center; background: blue";
+
+let box1Child = document.body.firstElementChild.children[1].firstElementChild;
+box1Child.style = "color: white; font-family: sans-serif;";
+box1Child.textContent = "Blue Box";
+
+// Target Box 2 && His Child
+let containerBox2 = document.body.firstElementChild.children[2];
+containerBox2.style = "text-align: center; background: red";
+
+let box2Child = document.body.firstElementChild.children[2].firstElementChild;
+box2Child.style = "color: white; font-family: sans-serif;";
+box2Child.textContent = "Red Box";
+
+// Target Box 3 && His Child
+let containerBox3 = document.body.firstElementChild.children[3];
+containerBox3.style = "text-align: center; background: green";
+
+let box3Child = document.body.firstElementChild.children[3].firstElementChild;
+box3Child.style = "color: white; font-family: sans-serif;";
+box3Child.textContent = "Green Box";
+
+// Target Box 4 && His Child
+let containerBox4 = document.body.firstElementChild.children[4];
+containerBox4.style = "text-align: center; background: black";
+
+let box4Child = document.body.firstElementChild.children[4].firstElementChild;
+box4Child.style = "color: white; font-family: sans-serif;";
+box4Child.textContent = "Black Box";
+
+// Target Box 5 && His Child
+let containerBox5 = document.body.firstElementChild.children[5];
+containerBox5.style = "text-align: center; background: yellowgreen";
+
+let box5Child = document.body.firstElementChild.children[5].firstElementChild;
+box5Child.style = "color: white; font-family: sans-serif;";
+box5Child.textContent = "Yellowgreen Box";
+
+// Target LasChild
+let containerLastChild = document.body.firstElementChild.lastElementChild;
+containerLastChild.style = "font-family: sans-serif";
+containerLastChild.textContent = "My name is Talha.My Father name is Anwer Hussain.I study in Class 11.I am 18 year old.";
